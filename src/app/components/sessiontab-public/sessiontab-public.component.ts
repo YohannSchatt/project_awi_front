0ff94f69f54cc1b886d0b6a09287db1a4a6de0ab
yohann.schatt <yohann.schatt@etu.umontpellier.fr>
2024-11-01 19:01:47 +0100

    ajout texte homePageGestion et modification css session

--- src/app/components/sessiontab-public/sessiontab-public.component.ts
+++ src/app/components/sessiontab-public/sessiontab-public.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { Session } from '../../Model/SessionClass';
 
 
 @Component({
   selector: 'app-sessiontab-public',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './sessiontab-public.component.html',
   styleUrl: './sessiontab-public.component.scss'
 })
@@ -13,8 +14,32 @@ export class SessiontabPublicComponent {
 
   public tabSession!: Session[];
 
+  public sessionActuelle!: Session;
+
+  constructor() { }
+
   ngOnInit() {
     this.loadSession();
+    this.getSessionActuelle();
+    this.getNextSession();
+  }
+
+  private getSessionActuelle() {
+    const dateActuelle = new Date();
+    this.tabSession.forEach((session: Session) => {
+      if (session.dateDebut < dateActuelle && session.dateFin > dateActuelle) {
+        this.sessionActuelle = session;
+      }
+    });
+  }
+
+  private getNextSession() {
+    const dateActuelle = new Date();
+    this.tabSession.forEach((session: Session) => {
+      if (session.dateDebut > dateActuelle) {
+        this.sessionActuelle = session;
+      }
+    });
   }
 
   private loadSession() {
